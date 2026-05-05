8512653d37cf4558f2c8064dd46e97099bae8198
Rich Soni <rich@richsoni.com>
2026-05-05 00:00:50 -0400

    config fzf

--- config/lua/config/fzf.lua
+++ config/lua/config/fzf.lua
@@ -1,5 +1,14 @@
 local fzf = require("fzf-lua")
 
-fzf.setup({ "default" })
+fzf.setup({
+  winopts = {
+    height  = 0.3,
+    width   = 1.0,
+    row     = 1.0,
+    col     = 0.0,
+    border  = "none",
+    preview = { hidden = "hidden" },
+  },
+})
 
 vim.keymap.set("n", "<C-p>", fzf.files, { desc = "Find files" })
